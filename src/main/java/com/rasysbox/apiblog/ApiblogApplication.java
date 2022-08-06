1dbb9ebc48545d7f4cbf34b12ab630300eff8cc8
raulrobinson <rasysbox@hotmail.com>
2022-08-06 16:02:49 -0500

    four commit

---
+++ src/main/java/com/rasysbox/apiblog/ApiblogApplication.java
@@ -0,0 +1,13 @@
+package com.rasysbox.apiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasysbox/apiblog/ApiblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasysbox.apiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
